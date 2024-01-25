65bdd338f809e77bfdd9dc739ae8ebf286287e43
megupatil <megupatil@users.noreply.github.com>
2024-01-25 07:03:38 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="megupatil"
+original_name="python_test"
+original_urlname="python-test"
+original_description="Awesome python_test created by megupatil"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv python_test $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ python_test/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for python_test."""
+
+from python_test.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
